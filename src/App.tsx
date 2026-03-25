090b7c69f141db525da64783d361eeda0b2c5688
Celesta Maria Varghese <celestavarghese35@gmail.com>
2026-03-25 11:18:37 +0100

    Day 2: Implemented Movie type and dynamic rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import type { Movie } from "./types/movie";
 
-function App(){
-  return(
+const movies: Movie[] = [
+  {
+    id: 1,
+    title: "Inception",
+    poster_path: "",
+    release_date: "2010",
+    vote_average: 8.8,
+  },
+  {
+    id: 2,
+    title: "Interstellar",
+    poster_path: "",
+    release_date: "2014",
+    vote_average: 8.6,
+  },
+];
+
+function App() {
+  return (
     <div>
-      <h1>Movie App</h1>
+      <h1>🎬 Movie App</h1>
+
+      {movies.map((movie) => {
+        return <p key={movie.id}>{movie.title}</p>;
+      })}
     </div>
-  
   );
 }
 
