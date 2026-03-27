d21413159ad303a85da901e4fcf5a994756024a5
Celesta Maria Varghese <celestavarghese35@gmail.com>
2026-03-27 10:51:13 +0100

    Day 5: Added loading state, error handling and improved UI styling

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,42 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const getMovies = async () => {
-      const data = await fetchMovies();
-      setMovies(data);
+      try {
+        const data = await fetchMovies();
+        setMovies(data);
+      } catch (err) {
+        setError("Failed to fetch movies");
+      } finally {
+        setLoading(false);
+      }
     };
 
     getMovies();
   }, []);
 
   return (
-  <div className="app">
-    <h1 className="title">🎬 Movie App</h1>
+    <div className="app">
+      <h1 className="title">🎬 Movie App</h1>
 
-    <div className="movies-container">
-      {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
+      {/* Loading */}
+      {loading && <p>Loading movies...</p>}
+
+      {/* Error */}
+      {error && <p>{error}</p>}
+
+      {/* Movies */}
+      <div className="movies-container">
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
 }
 
 export default App;
