419e0af02ef5c10e78d64f3f0755d56752f1edf1
Celesta Maria Varghese <celestavarghese35@gmail.com>
2026-04-17 13:38:52 +0200

    Day 10: Implemented routing, pages, and navigation using React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,13 @@
-import { useEffect, useState } from "react";
-import { fetchMovies } from "./services/movieService";
-import type { Movie } from "./types/movie";
-import MovieCard from "./components/MovieCard";
-import Loader from "./components/Loader";
-import Button from "./components/Button";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import MovieDetails from "./pages/MovieDetails";
 
 function App() {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-  const [query, setQuery] = useState("");
-
-  useEffect(() => {
-    const getMovies = async () => {
-      try {
-        const data = await fetchMovies();
-        setMovies(data);
-      } catch {
-        setError("Failed to fetch movies");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    getMovies();
-  }, []);
-
-  const handleSearch = async () => {
-    if (!query) return;
-
-    try {
-      setLoading(true);
-      setError(null);
-      setMovies([]);
-
-      const data = await fetchMovies(query);
-      setMovies(data);
-    } catch {
-      setError("Failed to search movies");
-    } finally {
-      setLoading(false);
-    }
-  };
-
   return (
-    <div className="app">
-      <h1 className="title">🎬 Movie App</h1>
-
-      <div style={{ textAlign: "center", marginBottom: "20px" }}>
-        <input
-          type="text"
-          placeholder="Search movies..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") handleSearch();
-          }}
-        />
-
-        <Button text="Search" onClick={handleSearch} />
-      </div>
-
-      {loading && <Loader />}
-
-      {error && (
-        <p style={{ color: "red", textAlign: "center" }}>
-          ❌ {error}
-        </p>
-      )}
-
-      {!loading && movies.length === 0 && (
-        <p style={{ textAlign: "center" }}>
-          😕 No movies found
-        </p>
-      )}
-
-      {!loading && (
-        <div className="movies-container">
-          {movies.map((movie) => (
-            <MovieCard key={movie.id} movie={movie} />
-          ))}
-        </div>
-      )}
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movie/:id" element={<MovieDetails />} />
+    </Routes>
   );
 }
 
--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,5 +1,6 @@
 import type { Movie } from "../types/movie";
 import "./MovieCard.css";
+import { Link } from "react-router-dom";
 
 type Props = {
   movie: Movie;
@@ -7,17 +8,17 @@ type Props = {
 
 function MovieCard({ movie }: Props) {
   return (
-    <div className="card">
-  <img
-    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-    alt={movie.title}
-  />
+    <Link to={`/movie/${movie.id}`} className="card">
+      <img
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        alt={movie.title}
+      />
 
-  <div className="card-content">
-    <h3>{movie.title}</h3>
-    <p>⭐ {movie.vote_average}</p>
-  </div>
-</div>
+      <div className="card-content">
+        <h3>{movie.title}</h3>
+        <p>⭐ {movie.vote_average}</p>
+      </div>
+    </Link>
   );
 }
 
